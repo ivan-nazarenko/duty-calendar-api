52422251b4d94cf0986968fa9333202662981f2d
Ivan Nazarenko <nazarenkoivan@protonmail.com>
2021-02-09 20:31:46 +0200

    Configure ssl

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,6 +5,10 @@ export = {
    "username": process.env.DB_USERNAME,
    "password": process.env.DB_PASSWORD,
    "database": process.env.DB_NAME,
+   "ssl": {
+      "require": true,
+      "rejectUnauthorized": false
+   },
    "synchronize": true,
    "logging": false,
    "entities": [
