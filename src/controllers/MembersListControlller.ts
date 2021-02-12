2ea0409962ea948e41c06035829b7f29dc9270a1
Ivan Nazarenko <nazarenkoivan@protonmail.com>
2021-02-12 10:41:06 +0200

    MembersList refactoring

--- src/controllers/MembersListControlller.ts
+++ src/controllers/MembersListControlller.ts
@@ -1,81 +1,95 @@
-import { Request, Response } from "express";
+import { Request, Response, Next } from "express";
 import { getRepository } from "typeorm";
 import { validate } from "class-validator";
 import { MembersList } from "../entity/MembersList";
+import { HttpException } from "../exceptions/HttpException";
+import { Member } from "../entity/Member";
 
 class MembersListController {
 
-    static getMembersList = async (req: Request, res: Response) => {
-        const userId = res.locals.jwtPayload.userId;
-
-        const listRepository = getRepository(MembersList);
+    static getMembersList = async (req: Request, res: Response, next: Next) => {
         try {
+            const userId = res.locals.jwtPayload.userId;
+
+            const listRepository = getRepository(MembersList);
             const list = await listRepository.findOneOrFail(
                 {
                     relations: ["members", "dutyEvents"],
                     where: { userId: userId }
                 });
             res.send(list);
-        } catch (err) {
-            res.status(404).send("List not found");
+        } catch {
+            next(new HttpException(404, 'List not found'));
         }
     };
 
-    static newMembersList = async (req: Request, res: Response) => {
-        const userId = res.locals.jwtPayload.userId;
-        let { name, members } = req.body;
-
-        let list = new MembersList();
-        list.name = name;
-        list.userId = userId;
-        list.members = members;
-
-        const errors = await validate(list);
-        if (errors.length > 0) {
-            res.status(400).send(errors);
-            return;
-        }
-
-        const listRepository = getRepository(MembersList);
+    static newMembersList = async (req: Request, res: Response, next: Next) => {
         try {
-            await listRepository.save(list);
-        } catch (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
+            const userId = res.locals.jwtPayload.userId;
+            let { name, members } = req.body;
 
-        res.status(201).send(list);
+            let list = new MembersList();
+            list.name = name;
+            list.userId = userId;
+            list.members = members;
+
+            const errors = await validate(list);
+            if (errors.length > 0) {
+                throw new HttpException();
+            }
+
+            const listRepository = getRepository(MembersList);
+            try {
+                await listRepository.save(list);
+            } catch (error) {
+                throw new HttpException(400, 'List already exist');
+            }
+
+            res.status(201).send(list);
+        } catch (error) {
+            next(error);
+        }
     };
 
-    static editMembersList = async (req: Request, res: Response) => {
-        const userId = res.locals.jwtPayload.userId;
-        let { name, members } = req.body;
-
-        let list;
-        const listRepository = getRepository(MembersList);
+    static editMembersList = async (req: Request, res: Response, next: Next) => {
         try {
-            list = await listRepository.findOneOrFail({ where: { userId: userId } });
-        } catch {
-            res.status(404);
-        }
+            const userId = res.locals.jwtPayload.userId;
+            let { name, members } = req.body;
 
-        list.name = name;
-        list.members = members;
+            let list: MembersList;
+            const listRepository = getRepository(MembersList);
 
-        const errors = await validate(list);
-        if (errors.length > 0) {
-            res.status(400).send(errors);
-            return;
-        }
+            try {
+                list = await listRepository.findOneOrFail(
+                    {
+                        relations: ["members", "dutyEvents"],
+                        where: { userId: userId }
+                    });
+            } catch {
+                throw new HttpException(404);
+            }
 
-        try {
+            const memberRepository = getRepository(Member);
+
+            if (list.members?.length > 0) {
+                await memberRepository.remove(list.members);
+            }
+
+            list.name = name;
+            list.members = members;
+
+            const errors = await validate(list);
+            if (errors.length > 0) {
+                throw new HttpException();
+            }
+
             await listRepository.save(list);
-        } catch {
-            res.status(400).send("Incorrect data");
+
+            res.status(204).send();
+
+        } catch (error) {
+            next(error);
         }
-
-        res.status(204).send();
-
     };
 };
 
